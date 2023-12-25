0b27b3802185fcfa6db4728628cf0253381935b6
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-12-25 09:06:47 +0200

    Update and rename 03 Evaluation System.py to 03 Doctors Evaluation.py

--- pages/03 Doctors Evaluation.py
+++ pages/03 Doctors Evaluation.py
@@ -4,8 +4,8 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="GSB", page_icon="16.png")
 st.image("GSB Medium.png", width=120,)
 
-st.title("GSB Evaluation System")
-components.iframe("https://app.powerbi.com/view?r=eyJrIjoiNTgzMjVlNzUtYmI2NC00NjhkLWJhMjItYzJmZThlOWUzMzQzIiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9", width=800, height=600)
+st.title("GSB Doctors Evaluation")
+components.iframe("https://app.powerbi.com/view?r=eyJrIjoiY2RmNDc3NDctOGFmYi00MWFkLWE3YWEtMGQ1YTNhM2FiOWY2IiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9&embedImagePlaceholder=true", width=800, height=600)
 
 hide_st_style = """
             <style>
